64e5e73046ec0f95855627975049c003ccd59aff
Abelsileshie <sileshieabel@gmail.com>
2024-10-20 20:22:39 +0300

    addig the script for quote application

---
+++ dom-manipulation/script.js
@@ -0,0 +1,50 @@
+const quoteDisplay = document.getElementById("quoteDisplay");
+const newQuoteButton = document.getElementById("newQuote");
+const addQuoteForm = document.getElementById("addQuoteForm");
+const newQuoteText = document.getElementById("newQuoteText");
+const newQuoteCategory = document.getElementById("newQuoteCategory");
+
+let quotes = [
+  {
+    text: "The only way to do great work is to love what you do.",
+    category: "Motivation",
+  },
+  {
+    text: "Life is what happens to you while you're busy making other plans.",
+    category: "Inspirational",
+  },
+  {
+    text: "Life is what happens to you while you're busy making other plans.",
+    category: "Funny",
+  },
+];
+
+function showRandomQuote() {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  const randomQuote = quotes[randomIndex];
+
+  quoteDisplay.textContent = randomQuote.text;
+}
+
+function createAddQuoteForm() {
+  addQuoteForm.style.display = "block";
+}
+
+function addQuote() {
+  const newQuote = {
+    text: newQuoteText.value,
+    category: newQuoteCategory.value,
+  };
+
+  quotes.push(newQuote);
+  newQuoteText.value = "";
+  newQuoteCategory.value = "";
+  addQuoteForm.style.display = "none";
+  showRandomQuote();
+}
+
+newQuoteButton.addEventListener("click", showRandomQuote);
+addQuoteForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  addQuote();
+});
